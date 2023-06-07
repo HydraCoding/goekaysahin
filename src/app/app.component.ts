bac55f51bbd51618fa2bdbf0146be22b55be95bf
HydraCoding <goekay.sahin@gmx.net>
2023-06-07 17:57:44 +0200

    setup for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  images = ['code.jpg', 'background-index.jpg', 'path.jpg'];
-  headlines = [
-    'Bring engineering to the next level',
-    'Dont wait for it to get easier, get better',
-    'Lets solve togehter',
-  ];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setInterval(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
-}
+export class AppComponent {}
