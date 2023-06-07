718c968f727311dafe118e7cb0923fac7a44e51e
HydraCoding <goekay.sahin@gmx.net>
2023-06-07 17:46:37 +0200

    desgin headline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'goekaysahin';
+export class AppComponent implements OnInit {
+  images = ['code.jpg', 'background-index.jpg', 'path.jpg'];
+  headlines = [
+    'Bring engineering to the next level',
+    'Dont wait for it to get easier, get better',
+    'Lets solve togehter',
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setInterval(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 }
